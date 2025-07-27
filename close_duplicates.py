56918b3a560bc3ccb64bb1677eaae131a2c257d1
深黑森林 <deepdarkforest@sohu.com>
2025-07-27 20:43:51 +0800

    Update close_duplicates.py

--- close_duplicates.py
+++ close_duplicates.py
@@ -5,8 +5,8 @@ import json
 
 # 配置参数
 TOKEN = os.environ['GITHUB_TOKEN']  # 使用环境变量中的 token
-REPO_OWNER = "Deep-Dark-Forest"
-REPO_NAME = "test2"
+REPO_OWNER = "Meloong-Git"
+REPO_NAME = "PCL"
 GITHUB_GRAPHQL_URL = "https://api.github.com/graphql"
 DUPLICATE_LABEL = "重复"
 
